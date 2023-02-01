17328c852d261672ff23962a1b0b4a1011db3903
Hamish Knox <hrjkknox@gmail.com>
2023-02-01 22:24:08 +0000

    Add main functionality (#1)
    * Create linesToSections
    
    * Create convertMSection
    
    * Create convertCSection
    
    * Formatting
    
    * Create convertSection
    
    * Create extractData
    
    * Fix import
    
    * Add function import
    
    * Add expected output test data
    
    * Add test file
    
    * Add dummy unit test
    
    * Make testable
    
    * Add unit test
    
    * Create .gitignore
    
    * Delete cache files
    
    * Delete cache files from testdata
    
    * Delete src/countach/__pycache__ directory

---
+++ tests/testdata/output.py
@@ -0,0 +1,56 @@
+output = [
+	{
+		'Name': 'read_1',
+		'Long Identifier': '',
+		'Type': 'VALUE',
+		'Address': 16580608,
+		'Record Layout': 'Scalar_FLOAT32_IEEE',
+		'Maximum Difference': 0,
+		'Conversion Method': 'newvcu_CM_single',
+		'Lower Limit': -3.4e+38,
+		'Upper Limit': 3.4e+38
+	},
+	{
+		'Name': 'write_1',
+		'Long Identifier': '',
+		'Type': 'FLOAT32_IEEE',
+		'Conversion Method': 'newvcu_CM_single',
+		'Resolution': 0, 'Accuracy': 0,
+		'Lower Limit': -3.4e+38,
+		'Upper Limit': 3.4e+38,
+		'Address': 1073774596
+	},
+	{
+		'Name': 'write_2',
+		'Long Identifier': '',
+		'Type': 'UBYTE',
+		'Conversion Method': 'newvcu_CM_boolean',
+		'Resolution': 0,
+		'Accuracy': 0,
+		'Lower Limit': 0.0,
+		'Upper Limit': 1.0,
+		'Address': 1073774600
+	}, 
+	{
+		'Name': 'PwrM_flgKeyOnDelay',
+		'Long Identifier': '',
+		'Type': 'UBYTE',
+		'Conversion Method': 'newvcu_CM_boolean',
+		'Resolution': 0,
+		'Accuracy': 0,
+		'Lower Limit': 0.0,
+		'Upper Limit': 1.0,
+		'Address': 1073774592
+	},
+	{
+		'Name': 'PwrM_flgKeyOnRaw',
+		'Long Identifier': '',
+		'Type': 'UBYTE',
+		'Conversion Method':'newvcu_CM_boolean',
+		'Resolution': 0,
+		'Accuracy': 0,
+		'Lower Limit': 0.0,
+		'Upper Limit': 1.0,
+		'Address': 1073774593
+	}
+]
