419bbf95da1f0af21d07b81eca07a7cde70fe61d
Hamish Knox <hrjkknox@gmail.com>
2023-02-27 20:15:46 +0000

    Add type test data

--- tests/testdata/output.py
+++ tests/testdata/output.py
@@ -59,3 +59,54 @@ output = [
 		'Address': 1073774593
 	}
 ]
+
+types = {
+	"uint8": {
+		"Name": "uint8",
+		"Bits": 8,
+		"Signed": False,
+		"Encoding": "int"
+		},
+	"uint16": {
+		"Name": "uint16",
+		"Bits": 16,
+		"Signed": False,
+		"Encoding": "int"
+		},
+	"uint32": {
+		"Name": "uint32",
+		"Bits": 32,
+		"Signed": False,
+		"Encoding": "int"
+		},
+	"int8": {
+		"Name": "int8",
+		"Bits": 8,
+		"Signed": True,
+		"Encoding": "int"
+		},
+	"int16": {
+		"Name": "int16",
+		"Bits": 16,
+		"Signed": True,
+		"Encoding": "int"
+		},
+	"int32": {
+		"Name": "int32",
+		"Bits": 32,
+		"Signed": True,
+		"Encoding": "int"
+		},
+    "boolean": {
+		"Name": "boolean",
+		"Bits": 8,
+		"Signed": False,
+		"Encoding": "boolean"
+		},
+    "single": {
+		"Name": "single",
+		"Bits": 32,
+		"Signed": True,
+		"Encoding": "float"
+		}
+}
