f9afe66dfc88268f6fea535f909d525f7b04cb21
Hamish Knox <hrjkknox@gmail.com>
2023-02-27 20:22:09 +0000

    Update test to use new type dict system

--- tests/testdata/output.py
+++ tests/testdata/output.py
@@ -1,65 +1,3 @@
-output = [
-	{
-		'Category': 'Characteristic',
-		'Name': 'read_1',
-		'Long Identifier': '',
-		'VCU Type': 'VALUE',
-		'Address': 16580608,
-		'Record Layout': 'Scalar_FLOAT32_IEEE',
-		'Maximum Difference': 0,
-		'Type': 'single',
-		'Lower Limit': -3.4e+38,
-		'Upper Limit': 3.4e+38
-	},
-	{
-		'Category': 'Measurement',
-		'Name': 'write_1',
-		'Long Identifier': '',
-		'VCU Type': 'FLOAT32_IEEE',
-		'Type': 'single',
-		'Resolution': 0, 'Accuracy': 0,
-		'Lower Limit': -3.4e+38,
-		'Upper Limit': 3.4e+38,
-		'Address': 1073774596
-	},
-	{
-		'Category': 'Measurement',
-		'Name': 'write_2',
-		'Long Identifier': '',
-		'VCU Type': 'UBYTE',
-		'Type': 'boolean',
-		'Resolution': 0,
-		'Accuracy': 0,
-		'Lower Limit': 0.0,
-		'Upper Limit': 1.0,
-		'Address': 1073774600
-	}, 
-	{
-		'Category': 'Measurement',
-		'Name': 'PwrM_flgKeyOnDelay',
-		'Long Identifier': '',
-		'VCU Type': 'UBYTE',
-		'Type': 'boolean',
-		'Resolution': 0,
-		'Accuracy': 0,
-		'Lower Limit': 0.0,
-		'Upper Limit': 1.0,
-		'Address': 1073774592
-	},
-	{
-		'Category': 'Measurement',
-		'Name': 'PwrM_flgKeyOnRaw',
-		'Long Identifier': '',
-		'VCU Type': 'UBYTE',
-		'Type':'boolean',
-		'Resolution': 0,
-		'Accuracy': 0,
-		'Lower Limit': 0.0,
-		'Upper Limit': 1.0,
-		'Address': 1073774593
-	}
-]
-
 types = {
 	"uint8": {
 		"Name": "uint8",
@@ -110,3 +48,65 @@ types = {
 		"Encoding": "float"
 		}
 }
+
+output = [
+	{
+		'Category': 'Characteristic',
+		'Name': 'read_1',
+		'Long Identifier': '',
+		'VCU Type': 'VALUE',
+		'Address': 16580608,
+		'Record Layout': 'Scalar_FLOAT32_IEEE',
+		'Maximum Difference': 0,
+		'Type': types['single'],
+		'Lower Limit': -3.4e+38,
+		'Upper Limit': 3.4e+38
+	},
+	{
+		'Category': 'Measurement',
+		'Name': 'write_1',
+		'Long Identifier': '',
+		'VCU Type': 'FLOAT32_IEEE',
+		'Type': types['single'],
+		'Resolution': 0, 'Accuracy': 0,
+		'Lower Limit': -3.4e+38,
+		'Upper Limit': 3.4e+38,
+		'Address': 1073774596
+	},
+	{
+		'Category': 'Measurement',
+		'Name': 'write_2',
+		'Long Identifier': '',
+		'VCU Type': 'UBYTE',
+		'Type': types['boolean'],
+		'Resolution': 0,
+		'Accuracy': 0,
+		'Lower Limit': 0.0,
+		'Upper Limit': 1.0,
+		'Address': 1073774600
+	}, 
+	{
+		'Category': 'Measurement',
+		'Name': 'PwrM_flgKeyOnDelay',
+		'Long Identifier': '',
+		'VCU Type': 'UBYTE',
+		'Type': types['boolean'],
+		'Resolution': 0,
+		'Accuracy': 0,
+		'Lower Limit': 0.0,
+		'Upper Limit': 1.0,
+		'Address': 1073774592
+	},
+	{
+		'Category': 'Measurement',
+		'Name': 'PwrM_flgKeyOnRaw',
+		'Long Identifier': '',
+		'VCU Type': 'UBYTE',
+		'Type': types['boolean'],
+		'Resolution': 0,
+		'Accuracy': 0,
+		'Lower Limit': 0.0,
+		'Upper Limit': 1.0,
+		'Address': 1073774593
+	}
+]
