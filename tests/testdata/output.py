ef143b5f07425f8b1b20ce8273bbf1a1cddbb200
Hamish Knox <hrjkknox@gmail.com>
2023-03-24 23:26:02 +0000

    Add test data

---
+++ tests/testdata/output.py
@@ -0,0 +1,61 @@
+output = [
+	{
+		'Category': 'Characteristic',
+		'Name': 'read_1',
+		'Long Identifier': '',
+		'VCU Type': 'VALUE',
+		'Address': 16580608,
+		'Record Layout': 'Scalar_FLOAT32_IEEE',
+		'Maximum Difference': 0,
+		'Type': 'single',
+		'Lower Limit': -3.4e+38,
+		'Upper Limit': 3.4e+38
+	},
+	{
+		'Category': 'Measurement',
+		'Name': 'write_1',
+		'Long Identifier': '',
+		'VCU Type': 'FLOAT32_IEEE',
+		'Type': 'single',
+		'Resolution': 0, 'Accuracy': 0,
+		'Lower Limit': -3.4e+38,
+		'Upper Limit': 3.4e+38,
+		'Address': 1073774596
+	},
+	{
+		'Category': 'Measurement',
+		'Name': 'write_2',
+		'Long Identifier': '',
+		'VCU Type': 'UBYTE',
+		'Type': 'boolean',
+		'Resolution': 0,
+		'Accuracy': 0,
+		'Lower Limit': 0.0,
+		'Upper Limit': 1.0,
+		'Address': 1073774600
+	}, 
+	{
+		'Category': 'Measurement',
+		'Name': 'PwrM_flgKeyOnDelay',
+		'Long Identifier': '',
+		'VCU Type': 'UBYTE',
+		'Type': 'boolean',
+		'Resolution': 0,
+		'Accuracy': 0,
+		'Lower Limit': 0.0,
+		'Upper Limit': 1.0,
+		'Address': 1073774592
+	},
+	{
+		'Category': 'Measurement',
+		'Name': 'PwrM_flgKeyOnRaw',
+		'Long Identifier': '',
+		'VCU Type': 'UBYTE',
+		'Type':'boolean',
+		'Resolution': 0,
+		'Accuracy': 0,
+		'Lower Limit': 0.0,
+		'Upper Limit': 1.0,
+		'Address': 1073774593
+	}
+]
