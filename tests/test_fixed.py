eb4d8c7d987e423744b4a0ebad00f5b916964d90
Hamish Knox <hrjkknox@gmail.com>
2023-03-24 22:59:03 +0000

    Add dummy tests

---
+++ tests/test_fixed.py
@@ -0,0 +1,22 @@
+from countach import fixed
+
+def test_extractInitialInt():
+    pass
+
+def test_checkIfFixedIsSigned():
+    pass
+
+def test_getWordLength():
+    pass
+
+def test_getTotalSlope():
+    pass
+
+def test_getBias():
+    pass
+
+def test_getFixedTypeParameters():
+    pass
+
+def test_decodeFixedTypeString():
+    pass
