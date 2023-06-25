e83e11e75f8049f806eab302cd89a6faa8979711
Hamish Knox <hrjkknox@gmail.com>
2023-06-25 21:00:10 +0100

    Implement MemorySegment class

---
+++ src/countach/memorySegment.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+@dataclass
+class MemorySegment:
+    name: str
+    longIdentifier: str
+    programType: str
+    memoryType: str
+    attribute: str
+    address: int
+    size: int
+    offset: Tuple[int, int, int, int, int]
+
