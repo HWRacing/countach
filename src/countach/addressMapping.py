c7004b77c1c2454bdba843393a7d4ea60cb0ebe7
Hamish Knox <hrjkknox@gmail.com>
2023-06-25 21:08:20 +0100

    Formatting

--- src/countach/addressMapping.py
+++ src/countach/addressMapping.py
@@ -2,6 +2,6 @@ from dataclasses import dataclass
 
 @dataclass
 class AddressMapping:
-    originalAddress: int
-    mappingAddress: int
-    length: int
+	originalAddress: int
+	mappingAddress: int
+	length: int
