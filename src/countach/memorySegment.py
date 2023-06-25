d5054be6636f0960023482f6ad23a973d3690758
Hamish Knox <hrjkknox@gmail.com>
2023-06-25 21:08:05 +0100

    Add mapping to memory segment

--- src/countach/memorySegment.py
+++ src/countach/memorySegment.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
 from typing import Tuple
+from countach import addressMapping
 
 @dataclass
 class MemorySegment:
-    name: str
-    longIdentifier: str
-    programType: str
-    memoryType: str
-    attribute: str
-    address: int
-    size: int
-    offset: Tuple[int, int, int, int, int]
+	name: str
+	longIdentifier: str
+	programType: str
+	memoryType: str
+	attribute: str
+	address: int
+	size: int
+	offset: Tuple[int, int, int, int, int]
+	mapping: addressMapping.AddressMapping
 
